1c17ca08a2e0af1defb7e414a867f16ae4d5c917
edgesean <43985841+edgesean@users.noreply.github.com>
2020-09-18 10:55:01 +0200

    Setting up mvc
    setting up mvc and connecting to SQL server.

---
+++ CommandsDictionaryApp/CommandsDictionary/Controllers/CommandsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CommandsDictionary.Data;
+using CommandsDictionary.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsDictionary.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly MockCommanderRepo _repository = new MockCommanderRepo();
+        //get api/commands
+        [HttpGet]
+        public ActionResult <IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetAppCommands();
+            return Ok(commandItems);
+        }
+
+        //GET api/commands/5
+        [HttpGet("{id}")]
+        public ActionResult <Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ CommandsDictionaryApp/CommandsDictionary/Data/MockCommanderRepo.cs
@@ -0,0 +1,29 @@
+﻿using CommandsDictionary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommandsDictionary.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAppCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle and pan" },
+                new Command { Id = 0, HowTo = "Make a cake", Line = "Mix ingrediants", Platform = "Bowl and spoon" },
+                new Command { Id = 0, HowTo = "Grill a steak", Line = "Heat grill", Platform = "Grill" }
+        };
+
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle and pan" };
+
+        }
+    }
+}
